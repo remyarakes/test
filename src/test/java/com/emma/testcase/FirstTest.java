8fe75a3452d1586c8875a8952a75cf981b582597
Remya KB <remyarakes@gmail.com>
2018-10-04 22:38:55 +0100

    First commit for test project

---
+++ src/test/java/com/emma/testcase/FirstTest.java
@@ -0,0 +1,28 @@
+package com.emma.testcase;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
+
+/**
+ * 
+ * @author remya
+ *
+ */
+public class FirstTest {
+	
+	WebDriver driver;
+
+	@BeforeTest
+	public void init(){
+		System.out.println("Here is my first test");
+		System.out.println("os name..." + System.getProperty("os.name").toLowerCase());
+		System.out.println(" user dir... " + System.getProperty("user.dir"));
+		String chromeDriverPath = System.getProperty("user.dir") + "/src/main/resources/com/emma/driver/chromedriver";
+		System.setProperty("webdriver.chrome.driver", chromeDriverPath);
+		driver = new ChromeDriver();
+		driver.get("https://www.google.co.uk");
+	}
+	
+	
+}
